d9ed501e93a1266194c14e02d9d200b901be350e
Pedro <pedrocch@fanbot.me>
2015-09-02 12:11:08 -0500

    *
    Signed-off-by: Pedro <pedrocch@fanbot.me>

--- resources/fnbt_editor.php
+++ resources/fnbt_editor.php
@@ -6,7 +6,7 @@
 	                        <h4 class="widget-h">Editor de Fanbot</h4>
 	                    </div>
 	                    <div class="col-xs-4 btn-add-task col-sm-2">
-                        	<button type="submit" class="btn btn-default btn-primary"><i class="fa fa-plus"></i>Agregar Fanbot</button>
+                        	<button type="submit" class="btn btn-default btn-primary btn-xs"><i class="fa fa-plus" style="padding: 0 30px"></i>Agregar Fanbot</button>
                     	</div>
                         <table class="table  table-hover general-table">
                             <thead>
