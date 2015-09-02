a131a24710b9e2114e1a8523c0cf7a99355007a8
Pedro <pedrocch@fanbot.me>
2015-09-02 16:45:07 -0500

    *
    Signed-off-by: Pedro <pedrocch@fanbot.me>

--- resources/fnbt_editor.php
+++ resources/fnbt_editor.php
@@ -117,6 +117,95 @@ function fnbtAction(name){
 </script> 
  <!-- Modal that configures a Fanbot facebook page -->
 
-    	<?php require_once("listModal.php"); ?>
+<script>
+    function callModal(fanbotName) {
+	    localStorage.setItem("fanbotName", fanbotName);
+		$('#configModal').modal('show');
+	     document.getElementById('hiddenField').value = fanbotName;
+	}
+
+    function changeImage() {
+        image_url = "https://graph.facebook.com/"+ document.getElementById('facebookUrl').value +"/picture";
+	    $.get(image_url)
+		.done(function() { 
+		// Do something now you know the image exists.
+            document.getElementById("fbImg").src = image_url;
+            $('#modalAlert').hide()
+			$('#cambiarBtn').prop('disabled', false);
+
+    	}).fail(function() { 
+            $('#modalAlert').show()
+			$('#cambiarBtn').prop('disabled', true);
+
+        // Image doesn't exist - do something else.
+    })
+
+    }
+    
+
+	  function mySubmit() {
+	     document.getElementById("myForm").submit();
+	   }
+</script>
+
+		  <div class="modal fade" id="configModal" role="dialog">
+		    <div class="modal-dialog">
+		    
+		      <!-- Modal content-->
+		      <div class="modal-content">
+		        <div class="modal-header">
+		          <button type="button" class="close" data-dismiss="modal">&times;</button>
+		          <h4 class="modal-title">Configura tu Fanbot</h4>
+		        </div>
+		        <div class="modal-body">
+					<form class="form-inline" action="change_page.php" method="get" id="formUrl">
+<! Fanbot Id label ->
+					  <div class="form-group">
+							<label for="fanbotId" class="control-label">ID</label>
+							<input type="text" class="form-control input-sm" id="fanbotId" placeholder="xxx-xxxx-xxx" name="fanbotId">
+					  </div>
+
+<! Fanbot Name label ->
+					  <div class="form-group">
+							<label for="fanbotName" class="control-label">Nombre</label>
+							<input type="text" class="form-control input-sm" id="fanbotId" placeholder="XXXX" name="fanbotName">
+					  </div>
+
+<! Fanbot client label ->
+					  <div class="form-group">
+							<label for="fanbotClient" class="control-label">Cliente</label>
+							<input type="text" class="form-control input-sm" id="fanbotId" placeholder="xxx-xxxx-xxx" name="fanbotClient">
+					  </div>
+
+<! Fanbot particle ID label ->
+					  <div class="form-group">
+							<label for="particleId" class="control-label">Particle ID</label>
+							<input type="text" class="form-control input-sm" id="fanbotId" placeholder="ID de particle" name="particleId">
+					  </div>
+
+					  <div class="form-group">
+						<input class="form-controlinput-sm" type='hidden' id= 'hiddenField' name='name' value='' />				  
+					  </div>
+			
+		        </div>
+
+			    <div class="modal-footer">
+				    <div class="center-block">
+			            <p style="text-align: center;">Aquí aparecerá la imagen de tu pagina.</p>	            
+						<img id="fbImg" src="https://graph.facebook.com/fanbotme/picture" class="img-responsive img-thumbnail center-block" alt="Cinque Terre">
+					</div>
+					<div id="modalAlert" class="alert alert-danger fade-in text-center" style="display: none; margin: 5px;">
+					  <strong>La pagina de Facebook escrita no existe. </strong> 
+					</div>
+					<p>
+					<button type="submit" id="cambiarBtn" class="btn btn-primary btn-sm" >Cambiar</button>
+					</p>
+					</form>		
+		        </div>
+		      
+	      	  </div>
+		    </div>
+		  </div>
+
 
 <!-- End modal --> 
