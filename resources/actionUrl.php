b72dcea4f86dd66c7c51659d32f9b6fae382a2a7
Pedro <pedrocch@fanbot.me>
2015-09-07 12:03:03 -0500

    *
    Signed-off-by: Pedro <pedrocch@fanbot.me>

--- resources/actionUrl.php
+++ resources/actionUrl.php
@@ -1,53 +1,29 @@
 <?php
-if (isset($_POST['nbmnbm'])) {
 
-		require(realpath(dirname(__FILE__) . "/./config.php"));
-    	$servername = $config["db"]["fanbot"]["host"];
-		$username = $config["db"]["fanbot"]["username"];
-		$password = $config["db"]["fanbot"]["password"];
-		$dbname = $config["db"]["fanbot"]["dbname"];
+if (isset($_SESSION["userId"])){
 
-		
+			echo functionNode();
 			
-		// Create connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
-		// Check connection
-		if ($conn->connect_error) {
-		    die("Connection failed: " . $conn->connect_error);
+		}
+
+
+function functionNode(){
+
+		if (isset($_POST['particleId'])){ 
+			return 1;
 		}
 		
-		$sql = "SELECT * FROM fanbot WHERE name = '". $_POST['name'] ."' ";
-		$result = $conn->query($sql);
-
-		$conn->close();
+		if (isset($_POST['paidStatus'])) { 
+			return 2;
+		}
 		
-		if ($result->num_rows > 0) {		    
-		    while($row = $result->fetch_assoc()) {
-			    			        
-		        $accesToken = $row["accesToken"];
-		        $deviceId = $row["deviceId"];
-
-			    }
-			 fanbotAction($deviceId, $accesToken);
-			 			    
-			} else {
+		if (isset($_POST['paidStatus'])){
+			
+		}
+}
 
 
-			}
-	}		
 
-	function fanbotAction($deviceId, $accesToken){
-		
-		$ip = 'api.particle.io';	
-				$ch = curl_init("https://". $ip ."/v1/devices/". $deviceId.  "/led?access_token=". $accesToken);
-				curl_setopt($ch, CURLOPT_POST, 1);
-				curl_setopt($ch, CURLOPT_POSTFIELDS, "params=D7,HIGH");
-				curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-				$output = curl_exec($ch);
-				curl_close($ch);
-	
-	}
-	
-	echo 'hola';
+
+
 ?>
