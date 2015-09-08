2f99d1ff4a673844924c20448b30f7bd35fd7826
Pedro <pedrocch@fanbot.me>
2015-09-08 16:19:07 -0500

    *
    Signed-off-by: Pedro <pedrocch@fanbot.me>

---
+++ resources/config.php
@@ -0,0 +1,23 @@
+<?php
+	
+// Facebook app id $config["fbApp"]["appId"];
+// Facebook app secret $config["fbApp"]["appSecret"];
+// Base url id $config["urls"]["baseUrl"];
+// Facebook app secret $config["fbApp"]["appSecret"];
+ 
+ 
+$config = array(
+    "db" => array(
+        "fanbot" => array(
+            "dbname" => "fanbot_db",
+            "username" => "Dev",
+            "password" => "\"TRFBMIsCWh{19",
+            "host" => "10.132.69.95",
+        )
+    ),
+    "urls" => array(
+        "baseUrl" => "http://45.55.50.134",
+    ),
+    "paths" => array(
+        )
+    );
