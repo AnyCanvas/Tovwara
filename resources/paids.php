85feb81329875adcb735e09bfbb00163a7caf60e
Pedro <pedrocch@fanbot.me>
2015-09-08 11:05:42 -0500

    *
    Signed-off-by: Pedro <pedrocch@fanbot.me>

--- resources/paids.php
+++ resources/paids.php
@@ -137,7 +137,7 @@ function fnbtAction(){
 
     function callModal(fanbotId, fanbotPlan,courtDate,freeMonth,paidStatus) {
 	    localStorage.setItem("fanbotId", fanbotId);
-	    $("select#fanbotPlan").val("03");
+	    $("select#fanbotPlan").val("0" + fanbotPlan);
 	    $("select#courtDate").val(courtDate);
 	    $("select#freeMonth").val(freeMonth);
 	    $("select#paidStatus").val(paidStatus);
