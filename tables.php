9cb955dbe6b8950a639b9682072067b94109bded
Pedro <pedrocch@fanbot.me>
2015-09-18 15:20:59 -0500

    *
    Signed-off-by: Pedro <pedrocch@fanbot.me>

--- tables.php
+++ tables.php
@@ -106,7 +106,7 @@
     $('#usersTable').DataTable({
 	language: {
 	        url: 'https://cdn.datatables.net/plug-ins/1.10.9/i18n/Spanish.json'
-	    }
+	    },
 	"pageLength": 100	    
     });
 } );
