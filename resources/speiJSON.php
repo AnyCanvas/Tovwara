0c631b041277a0665b1776c806332fdf32f267fe
Pedro <pedrocch@fanbot.me>
2016-03-08 12:07:22 -0600

    *

--- resources/speiJSON.php
+++ resources/speiJSON.php
@@ -4,24 +4,24 @@
 	Conekta::setApiKey("key_nvJm2aBVNEd1qxPwxzzwrA");
 	
 	$charge = Conekta_Charge::create(array(
-		  'description'=> 'Stogies',
-		  'reference_id'=> '9839-wolf_pack',
-		  'amount'=> 20000,
+		  'description'=> 'Fanbot Plan',
+		  'reference_id'=> '01',
+		  'amount'=> 750,
 		  'currency'=>'MXN',
 		  'bank'=> array(
 		    'type'=> 'spei'
 		  ),
 		  'details'=> array(
-		    'name'=> 'Arnulfo Quimare',
-		    'phone'=> '403-342-0642',
-		    'email'=> 'logan@x-men.org',
+		    'name'=> 'Juan Pedro Casillas',
+		    'phone'=> '3311703904',
+		    'email'=> 'pedrocch@gmail.com',
 		    'customer'=> array(
-		      'logged_in'=> true,
-		      'successful_purchases'=> 14,
-		      'created_at'=> 1379784950,
-		      'updated_at'=> 1379784950,
-		      'offline_payments'=> 4,
-		      'score'=> 9
+//		      'logged_in'=> true,
+//		      'successful_purchases'=> 14,
+//		      'created_at'=> 1379784950,
+//		      'updated_at'=> 1379784950,
+//		      'offline_payments'=> 4,
+//		      'score'=> 9
 		    ),
 		    'line_items'=> array(
 		      array(
