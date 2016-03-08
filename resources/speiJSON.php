22ff839548bd44d88944a32389c18f39e4a8eeb9
Pedro <pedrocch@fanbot.me>
2016-03-08 12:18:15 -0600

    *

--- resources/speiJSON.php
+++ resources/speiJSON.php
@@ -6,7 +6,7 @@
 	$charge = Conekta_Charge::create(array(
 		  'description'=> 'Fanbot Plan',
 		  'reference_id'=> '01',
-		  'amount'=> 1000,
+		  'amount'=> 75000,
 		  'currency'=>'MXN',
 		  'bank'=> array(
 		    'type'=> 'spei'
