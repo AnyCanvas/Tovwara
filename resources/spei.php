bba40ca14867139c35b72ed1378a5a644961ff68
Pedro <pedrocch@fanbot.me>
2016-03-09 13:36:43 -0600

    *

--- resources/spei.php
+++ resources/spei.php
@@ -9,27 +9,27 @@
                                 <form role="form" action="resources/speiJSON.php" method="post" target="_blank">
                                 <div class="form-group">
                                     <label for="name">Nombre</label>
-                                    <input type="text" class="form-control" id="name" placeholder="Nombre del cliente" required>
+                                    <input type="text" class="form-control" name="name" placeholder="Nombre del cliente" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="email">Email</label>
-                                    <input type="email" class="form-control" id="email" placeholder="Email del cliente" required>
+                                    <input type="email" class="form-control" name="email" placeholder="Email del cliente" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="concept">Concepto</label>
-                                    <input type="text" class="form-control" id="concept" placeholder="Concepto de la compra" required>
+                                    <input type="text" class="form-control" name="concept" placeholder="Concepto de la compra" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="amount">Cantidad</label>
-                                    <input type="number" class="form-control" id="amount" placeholder="Cantidad a pagar" required>
+                                    <input type="number" class="form-control" name="amount" placeholder="Cantidad a pagar" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="xmlFile">XML de factura</label>
-                                    <input type="file" id="xmlFile" required>
+                                    <input type="file" name="xmlFile" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="pdfFile">PDF de factura</label>
-                                    <input type="file" id="pdfFile" required>
+                                    <input type="file" name="pdfFile" required>
                                 </div>
 
                                 <button type="submit" class="btn btn-info">Enviar</button>
