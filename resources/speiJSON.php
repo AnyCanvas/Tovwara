4b4154ce75768f7879a554964e4f16d921a30278
Pedro <pedrocch@fanbot.me>
2016-03-10 11:03:58 -0600

    *

--- resources/speiJSON.php
+++ resources/speiJSON.php
@@ -58,4 +58,48 @@
 //		echo ',';
 //		echo'"CLABE":"'. $charge->payment_method->clabe . '"';
 //		echo '}';
+
+
+	$url = 'https://api.sendgrid.com/';
+	$user = 'desarrollo@fanbot.me';
+	$pass = 'R3GVcDFWYCQ7Ap';
+	
+	$fileName = 'factura.xml';
+
+	$params = array(
+	    'api_user'  => $user,
+	    'api_key'   => $pass,
+	    'to'        => $email,
+	    'subject'   => 'test of file sends',
+	    'html'      => '<p> the HTML </p>',
+	    'text'      => 'the plain text',
+	    'from'      => 'example@sendgrid.com',
+	    'files['.$fileName.']' => '@'.$_FILES['photo']['tmp_name']
+	  );
+	
+	print_r($params);
+	
+	$request =  $url.'api/mail.send.json';
+	
+	// Generate curl request
+	$session = curl_init($request);
+	
+	// Tell curl to use HTTP POST
+	curl_setopt ($session, CURLOPT_POST, true);
+	
+	// Tell curl that this is the body of the POST
+	curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+	
+	// Tell curl not to return headers, but do return the response
+	curl_setopt($session, CURLOPT_HEADER, false);
+	// Tell PHP not to use SSLv3 (instead opting for TLS)
+	curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+	curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+	
+	// obtain response
+	$response = curl_exec($session);
+	curl_close($session);
+	
+	// print everything out
+	print_r($response);
 ?>
