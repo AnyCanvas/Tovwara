c538fb659c16826c422565b7f957b944e27d0379
Pedro <pedrocch@fanbot.me>
2016-03-10 13:43:50 -0600

    *

--- resources/speiJSON.php
+++ resources/speiJSON.php
@@ -67,7 +67,7 @@
 	    'html'      => '<p> the HTML </p>',
 	    'text'      => 'the plain text',
 	    'from'      => 'Ventas Fanbot <pedrocch@fanbot.me>',
-	    'attachment'[1] => '@'.$_FILES['xmlfile']['name']
+	    'attachment' => '@'.$_FILES['xmlfile']['tmp_name']
 	  );
 	
 	print_r($params);
