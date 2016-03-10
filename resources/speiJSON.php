485b8cb39b3bd194e9085795519a6f7424a1c6f9
Pedro <pedrocch@fanbot.me>
2016-03-10 11:16:12 -0600

    *

--- resources/speiJSON.php
+++ resources/speiJSON.php
@@ -6,6 +6,15 @@
 	$email = $_POST["email"];
 	$conept = $_POST["concept"];
 	$amount = $_POST["amount"];
+	
+	if (move_uploaded_file($_FILES['xmlfile']['tmp_name'], '/var/www/html' . $_FILES['xmlfile']['name'])) {
+	    echo "El fichero es válido y se subió con éxito.\n";
+	} else {
+	    echo "¡Posible ataque de subida de ficheros!\n";
+	}
+	
+	echo 'Más información de depuración:';
+	print_r($_FILES);
 
 	
 //	$charge = Conekta_Charge::create(array(
