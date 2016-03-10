a16971ce49537e7f332a558ef4c3d5acd1688ea3
Pedro <pedrocch@fanbot.me>
2016-03-10 11:32:14 -0600

    *

--- resources/speiJSON.php
+++ resources/speiJSON.php
@@ -7,7 +7,7 @@
 	$concept = $_POST["concept"];
 	$amount = $_POST["amount"];
 	
-	if (move_uploaded_file($_FILES['xmlfile']['tmp_name'], '/var/www/html/' . $_FILES['xmlfile']['name'])) {
+	if (move_uploaded_file($_FILES['xmlfile']['tmp_name'], '/var/www/html/factura.xml')) {
 	    echo "El fichero es válido y se subió con éxito.\n";
 	} else {
 	    echo "¡Posible ataque de subida de ficheros!\n";
