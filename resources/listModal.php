ee2982b1640c5b40da8be9e5ca860d05d2f8279a
Pedro <pedrocch@fanbot.me>
2016-03-22 12:47:50 -0600

    *

--- resources/listModal.php
+++ resources/listModal.php
@@ -65,7 +65,7 @@
 					  </div>
 					  <div class="form-group">
 							<label class="checkbox-inline">
-                                <input type="checkbox" id="action" value="1"> Check-in
+                                Check-in <input type="checkbox" id="action" value="1">
                             </label>
 					  </div>
 					  <a onclick="changeImage()" class="btn btn-default btn-xs">Verificar</a>						
