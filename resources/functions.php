79ce38bd14c4750f38dec25603f06bb4b898a798
Pedro <pedrocch@fanbot.me>
2016-05-10 12:14:32 -0500

    *

--- resources/functions.php
+++ resources/functions.php
@@ -234,9 +234,9 @@ function listUsers(){
 		}
 		
 		if ( $_SESSION['userId'] == 00){
-			$sql = "SELECT COUNT(t2.fbId), t1.id, t2.firstName,t2.lastName, t2.fbId, t2.email, t2.gender FROM interactions t1, users t2 WHERE t1.userId = t2.fbID GROUP BY t2.fbId;";
+			$sql = "SELECT COUNT(t2.fbId), t2.id, t2.firstName,t2.lastName, t2.fbId, t2.email, t2.gender FROM interactions t1, users t2 WHERE t1.userId = t2.fbID GROUP BY t2.fbId;";
 			}else{
-			$sql = "SELECT COUNT(t2.fbId), t1.id, t2.firstName,t2.lastName, t2.fbId, t2.email, t2.gender FROM interactions t1, users t2 WHERE t1.userId = t2.fbID AND t1.clientId=" . $_SESSION['userId']. "  GROUP BY t2.fbId;";
+			$sql = "SELECT COUNT(t2.fbId), t2.id, t2.firstName,t2.lastName, t2.fbId, t2.email, t2.gender FROM interactions t1, users t2 WHERE t1.userId = t2.fbID AND t1.clientId=" . $_SESSION['userId']. "  GROUP BY t2.fbId;";
 
 		}
 		$result = $conn->query($sql);
