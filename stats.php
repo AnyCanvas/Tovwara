8fc58a153257197a0ff06e769e7aa58d203e8e75
Pedro <pedrocch@fanbot.me>
2016-05-13 11:36:00 -0500

    *

--- stats.php
+++ stats.php
@@ -87,7 +87,7 @@
            <div class="col-sm-6">
                <section class="panel">
                    <header class="panel-heading">
-                       Sexo
+                       Likes/Checkin 
                    <span class="tools pull-right">
                        <a href="javascript:;" class="fa fa-chevron-down"></a>
                     </span>
