d9bd81c3d8ca6cdaeac1fd4fe6e58783bca758ff
Pedro <pedrocch@fanbot.me>
2016-05-16 10:30:57 -0500

    *

--- resources/fnbt_editor.php
+++ resources/fnbt_editor.php
@@ -52,6 +52,11 @@
 								                                <td><kbd class="text-uppercase"><?php echo $row['name']?></kbd></td>
 								                                <td><?php echo $row['clientId']?></td>
 								                                <td><?php echo $row['deviceId']?></td>
+																<td>
+									                            <a class="btn btn-primary btn-xs" onclick="callModal('<?php echo $row['id']?>')">
+										                            <span class="fa fa-cog" aria-hidden="true"></span> Configurar
+										                            </a>
+									                            </td>								                                
 								                            </tr>
 								
 								
@@ -85,6 +90,10 @@
 		$('#configModal').modal('show');
 	}
 
+    function callInteractionModal() {
+		$('#actionModal').modal('show');
+	}
+
 	function addFnbt(){
 
 	        var ajaxurl = 'resources/actionUrl.php';
@@ -101,7 +110,23 @@
 	        });
 			$('#configModal').modal('hide');
 	    }   
-    
+
+	function listAction(){
+
+	        var ajaxurl = 'resources/actionUrl.php';
+	        var fanbotId = $("#fanbotId").val();
+	        var fanbotName = $( "#fanbotName" ).val();
+	        var fanbotClient = $( "#fanbotClient" ).val();
+	        var particleId = $( "#particleId" ).val();
+
+	        data =  {'fanbotId' : fanbotId, 'fanbotName': fanbotName, 'fanbotClient' : fanbotClient, 'particleId': particleId};
+	        console.log(data);
+	        $.post(ajaxurl, data, function (response) {
+	            // Response div goes here.
+	            console.log(response);
+	        });
+			$('#configModal').modal('hide');
+	    }       
 </script>
 
 		  <div class="modal fade" id="configModal" role="dialog">
@@ -170,6 +195,59 @@
 							<input type="text" class="form-control input-sm" id="particleId" placeholder="ID de particle" name="particleId">
 					  </div>
 					  					  					  					  
+					  </form>					
+		        </div>
+
+			    <div class="modal-footer">
+					<button id="cambiarBtn" class="btn btn-primary btn-sm" onclick="addFnbt()">Terminar</button>
+
+		        </div>
+	      
+	      	  </div>
+		    </div>
+		  </div>
+
+
+<!-- End modal --> 
+
+
+		  <div class="modal fade" id="actionModal" role="dialog">
+		    <div class="modal-dialog">
+		      <!-- Modal content-->
+		      <div class="modal-content">
+		        <div class="modal-header">
+		          <button type="button" class="close" data-dismiss="modal">&times;</button>
+		          <h4 class="modal-title">Configura tu Fanbot</h4>
+		        </div>
+		        <div class="modal-body">
+
+				<form class="form" action="change_page.php" method="get" id="formUrl">
+
+<!-- Fanbot Id label -->
+					  <div class="form-group">
+							<label for="fanbotId" class="control-label">ID</label>
+							<input type="text" class="form-control input-sm" id="fanbotId" placeholder="xx-xx-xxx-xxxxx" name="fanbotId">
+					  </div>
+
+<!-- Fanbot Name label -->
+					  <div class="form-group">
+							<label for="fanbotName" class="control-label">Nombre</label>
+							<input type="text" class="form-control input-sm" id="fanbotName" placeholder="XXXX" name="fanbotName">
+					  </div>
+
+<!-- Fanbot client label -->
+					  <div class="form-group">
+							<div class="form-group">
+                                <label class="control-label col-md-3">Date Range</label>
+                                <div class="col-md-4">
+                                    <div class="input-group input-large" data-date="13/07/2013" data-date-format="mm/dd/yyyy">
+                                        <input type="text" class="form-control dpd1" name="from">
+                                        <span class="input-group-addon">To</span>
+                                        <input type="text" class="form-control dpd2" name="to">
+                                    </div>
+                                    <span class="help-block">Select date range</span>
+                                </div>
+                            </div>		  					  					  
 					  </form>					
 		        </div>
 
