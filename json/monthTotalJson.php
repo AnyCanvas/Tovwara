7e6dbe427ea0210e7b2655e1d35fdfa0dc00a1b2
Pedro <pedrocch@fanbot.me>
2016-05-16 11:41:04 -0500

    *

--- json/monthTotalJson.php
+++ json/monthTotalJson.php
@@ -1,7 +1,12 @@
 	<?php 
 		include "../resources/functions.php"; 		
 		if( isset($_POST["startDate"]) ){
-			totalJson(date("1"),date("2015"),$_GET["fnbtId"],0);
+			$string = $_POST["startDate"];
+			$timestamp = strtotime($string);
+			$m = date("m", $timestamp);
+			$y = date("y", $timestamp);
+
+			totalJson(date($m),date($y),$_GET["fnbtId"],0);
 		} else if( isset($_GET["fnbtId"]) && isset($_GET["clientId"])){
 			totalJson(date("m"),date("Y"),$_GET["fnbtId"],$_GET["clientId"]);				
 		} else if( isset($_GET["fnbtId"]) ){
