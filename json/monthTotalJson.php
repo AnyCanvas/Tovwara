4e9ec9759e2e9e1f327662958e3a9bfe09ba916c
Pedro <pedrocch@fanbot.me>
2016-05-16 11:47:33 -0500

    *

--- json/monthTotalJson.php
+++ json/monthTotalJson.php
@@ -6,6 +6,7 @@
 			$m = date("m", $timestamp);
 			$y = date("y", $timestamp);
 			echo ($m.$y);
+			echo $_POST["fanbotId"];
 			totalJson(date(5),date($y),$_POST["fanbotId"],0);
 		} else if( isset($_GET["fnbtId"]) && isset($_GET["clientId"])){
 			totalJson(date("m"),date("Y"),$_GET["fnbtId"],$_GET["clientId"]);				
