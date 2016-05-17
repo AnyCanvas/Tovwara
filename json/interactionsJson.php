4a543d9048d1a4ff8f47c637f9493a322f9d79a4
Pedro <pedrocch@fanbot.me>
2016-05-17 17:21:26 -0500

    *

--- json/interactionsJson.php
+++ json/interactionsJson.php
@@ -1,7 +1,7 @@
 	<?php 
 		include "../resources/functions.php"; 	
 
-		if($_SESSION['userId'] !== '00'){
+		if($_SESSION['userId'] !== 00){
 			likesJson(date("m"),date("Y"),0,$_SESSION['userId']);
 		} if( isset($_GET["fnbtId"]) && isset($_GET["clientId"])){
 			likesJson(date("m"),date("Y"),$_GET["fnbtId"],$_GET["clientId"]);				
