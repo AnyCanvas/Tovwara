7c112ad276bbd5ef1b0c8dba3551f66fbcd7c344
Pedro <pedrocch@fanbot.me>
2016-05-17 11:18:02 -0500

    *

--- specialStats.php
+++ specialStats.php
@@ -196,12 +196,11 @@
         legend: {
 	       hide: ['Total'],
 	    },
-	    axis: {
-	        x: {
-	            type: 'category',
-	            categories: ['cat1', 'cat2', 'cat3', 'cat4', 'cat5', 'cat6', 'cat7', 'cat8', 'cat9']
-	        }
-	    }
+		tooltip: {
+		  format: {
+		    title: "Totales"
+		  }
+		}
       });
     });
     $(function () {
