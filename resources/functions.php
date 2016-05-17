b4bf6dc02a350cef0b8bcee697b2bdcada105113
Pedro <pedrocch@fanbot.me>
2016-05-17 13:35:36 -0500

    *

--- resources/functions.php
+++ resources/functions.php
@@ -385,7 +385,7 @@ function listUsers(){
                         <th>Likes</th>
                         <th>Check-in</th>
                         <th>Acciones</th>
-                        <th>Fecha de primera acción</th>
+                        <th>Creado el:</th>
                     </tr>
                     </thead>
 
@@ -460,7 +460,7 @@ function listUsers(){
                         <th>Likes</th>
                         <th>Check-in</th>
                         <th>Acciones</th>
-                        <th>Fecha de primera acción</th>
+                        <th>Creado el:</th>
                     </tr>
                     </tfoot>
                     </table>
