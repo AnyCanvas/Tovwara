8a1335003abbd162c676014344bfdbd15ac08962
Pedro <pedrocch@fanbot.me>
2016-05-18 17:37:06 -0500

    *

--- json/monthTotalJson.php
+++ json/monthTotalJson.php
@@ -4,8 +4,21 @@
 		if (!isLogged()){
 			exit;
 		}			
+
+		if( isset($_GET["m"]) ){
+			$m = $_GET["m"];
+		} else {
+			$m = date("m");
+		}
+
+		if( isset($_GET["y"]) ){
+			$m = $_GET["y"];
+		} else {
+			$m = date("Y");
+		}
+
 		if($_SESSION['userId'] !== '00'){
-			totalJson(0,date("m"),date("Y"),0,$_SESSION['userId']);
+			totalJson(0,$m,$y,0,$_SESSION['userId']);
 		}  else if( isset($_POST["startDate"]) ){
 			$string = $_POST["startDate"];
 			$time = strtotime($string);
@@ -14,12 +27,12 @@
 			$y = date("Y", $time);
 			totalJson($d,$m,$y,$_POST["fanbotId"],0);
 		} else if( isset($_GET["fnbtId"]) && isset($_GET["clientId"])){
-			totalJson(0,date("m"),date("Y"),$_GET["fnbtId"],$_GET["clientId"]);				
+			totalJson(0,$m,$y,$_GET["fnbtId"],$_GET["clientId"]);				
 		} else if( isset($_GET["fnbtId"]) ){
-			totalJson(0,date("m"),date("Y"),$_GET["fnbtId"],0);				
+			totalJson(0,$m,$y,$_GET["fnbtId"],0);				
 		}else if( isset($_GET["clientId"]) ){
-			totalJson(0,date("m"),date("Y"),0,$_GET["clientId"]);				
+			totalJson(0,$m,$y,0,$_GET["clientId"]);				
 		} else{
-			totalJson(0,date("m"),date("Y"),0,0);
+			totalJson(0,$m,$y,0,0);
 		}
 	?> 
