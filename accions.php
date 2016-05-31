e257e1a672aeb02a5afcb8936de96fcddc50cf80
Pedro <pedrocch@fanbot.me>
2016-05-31 15:33:58 -0500

    *

--- accions.php
+++ accions.php
@@ -171,6 +171,17 @@
                 // `data` option, which defaults to the column being worked with, in
                 // this case `data: 0`.
                 "render": function ( data, type, row ) {
+	                var date = new Date(data * 1000).format('h:i:s')
+                    return date;
+                    
+                },
+                "targets": 0
+            },
+            {
+                // The `data` parameter refers to the data for the cell (defined by the
+                // `data` option, which defaults to the column being worked with, in
+                // this case `data: 0`.
+                "render": function ( data, type, row ) {
                     return '<a href="http://www.facebook.com/'+ row[4] +'" target="_blank">'+ data +'</a>';
                     
                 },
