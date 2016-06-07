7df236e2623b95979d5f3491e6952b76503d0dc4
Pedro <pedrocch@fanbot.me>
2016-06-07 16:28:34 -0500

    *

--- accions.php
+++ accions.php
@@ -78,23 +78,11 @@
 								    <input type="date" class="datepick" id="start-date"> a 
 								    <input type="date" class="datepicker" id="end-date">
 								  </div>
-								    <label for="pwd">Password:</label>
-								    <input type="password" class="form-control" id="pwd">
-								  <div class="checkbox">
-								    <label><input type="checkbox"> Remember me</label>
-								  </div>
 								</div>
 
 							    <label for="email">Email address:</label>
 							    <input type="email" class="form-control" id="email">
-							  </div>
-							  <div class="form-group">
-							    <label for="pwd">Password:</label>
-							    <input type="password" class="form-control" id="pwd">
-							  </div>
-							  <div class="checkbox">
-							    <label><input type="checkbox"> Remember me</label>
-							  </div>
+
 							  <button type="submit" class="btn btn-default">Submit</button>
 							</form>	                    
 
