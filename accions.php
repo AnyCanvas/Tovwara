c9dc16ba867dac75fff41d746de0271d7288dc0c
Pedro <pedrocch@fanbot.me>
2016-06-07 11:34:22 -0500

    *

--- accions.php
+++ accions.php
@@ -72,6 +72,21 @@
 
 							<form role="form">
 							  <div class="form-group">
+								<div class="form-inline">
+								  <div class="form-group">
+								    <label for="email">Email address:</label>
+								    <input type="email" class="form-control" id="email">
+								  </div>
+								  <div class="form-group">
+								    <label for="pwd">Password:</label>
+								    <input type="password" class="form-control" id="pwd">
+								  </div>
+								  <div class="checkbox">
+								    <label><input type="checkbox"> Remember me</label>
+								  </div>
+								  <button type="submit" class="btn btn-default">Submit</button>
+								</div>
+
 							    <label for="email">Email address:</label>
 							    <input type="email" class="form-control" id="email">
 							  </div>
