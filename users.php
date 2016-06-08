fdcaef1d2e7d746f8fb5ba5d33e256b033a1b3d8
Pedro <pedrocch@fanbot.me>
2016-06-08 14:51:52 -0500

    *

--- users.php
+++ users.php
@@ -265,7 +265,7 @@ $(document).ready( function () {
                 "render": function ( data, type, full, meta ) {
 					var utcSeconds = data;
 					var date = new Date(0); // The 0 there is the key, which sets the date to the epoch
-					date.setUTCSeconds(utcSeconds);	                
+					date.setUTCSeconds(utcSeconds);           
                     sort  = data=="" ? "" : data;
                     display = data=="" ? "" : date.getDay() + '/' + date.getMonth() + '/' + date.getFullYear();
 
