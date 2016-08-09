999c9bc726a0e9e238ddd350869ab502359bcea1
Pedro <pedrocch@fanbot.me>
2016-08-09 11:47:49 -0500

    *

--- resources/listFnbt.php
+++ resources/listFnbt.php
@@ -109,7 +109,7 @@
 																	</td>
 																	<td>
 																		<?php 
-																			  $timezone = $_SESSION['time'];
+																			  // $timezone = $_SESSION['time'];
 																			  $datetime = new DateTime($fanbotList[$key]["last_heard"]);
 																			  $orderDate = $datetime->format('ymd');
 																			  echo '<span style="display: none;">'. $orderDate .'</span>'. $datetime->format('d-m-Y H:i'); 
