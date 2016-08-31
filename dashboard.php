ba23cde5b4b5d111b547709c275fdbe8342f0603
Pedro <pedrocch@fanbot.me>
2016-08-31 18:09:43 -0500

    *

--- dashboard.php
+++ dashboard.php
@@ -108,19 +108,39 @@
                     </section>
                 </div>
             </div>
-            <div class="col-md-6">
-                <section class="panel weather-box">
-                    <div class="symbol purple-bg">
-                        <i class="ico-cloudy "></i>
-                    </div>
-                    <div class="value">
-                        <i class="fa fa-map-marker"></i>
-                        <p>California</p>
-                        <br>
-                        <i class="ico-cloudy"></i>
-                        <p>Sunny day, 32OC</p>
-                    </div>
-                </section>
+            <div class="col-md-3">
+                <div class="profile-nav alt">
+                    <section class="panel text-center">
+                        <div class="user-heading alt wdgt-row terques-bg">
+                            <i class="fa fa-user"></i>
+                        </div>
+
+                        <div class="panel-body">
+                            <div class="wdgt-value">
+                                <h1 class="count">25</h1>
+                                <p>New Users</p>
+                            </div>
+                        </div>
+
+                    </section>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="profile-nav alt">
+                    <section class="panel text-center">
+                        <div class="user-heading alt wdgt-row red-bg">
+                            <i class="fa fa-tags"></i>
+                        </div>
+
+                        <div class="panel-body">
+                            <div class="wdgt-value">
+                                <h1 class="count">295</h1>
+                                <p>Sales</p>
+                            </div>
+                        </div>
+
+                    </section>
+                </div>
             </div>
         </div>
                 
