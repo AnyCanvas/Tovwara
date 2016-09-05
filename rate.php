d60ad3d6784a50a601e9481e23815b2c867bfc6c
Pedro <pedrocch@fanbot.me>
2016-09-05 13:53:47 -0500

    *

--- rate.php
+++ rate.php
@@ -128,19 +128,6 @@
 <script src="js/jQuery-slimScroll-1.3.0/jquery.slimscroll.js"></script>
 <script src="js/jquery.nicescroll.js"></script>
 
-<!--Morris Chart-->
-<script src="js/morris-chart/morris.js"></script>
-<script src="js/morris-chart/raphael-min.js"></script>
-<!--Easy Pie Chart-->
-<script src="js/easypiechart/jquery.easypiechart.js"></script>
-<!--Sparkline Chart-->
-<script src="js/sparkline/jquery.sparkline.js"></script>
-<!--jQuery Flot Chart-->
-<script src="js/flot-chart/jquery.flot.js"></script>
-<script src="js/flot-chart/jquery.flot.tooltip.min.js"></script>
-<script src="js/flot-chart/jquery.flot.resize.js"></script>
-<script src="js/flot-chart/jquery.flot.pie.resize.js"></script>
-
 <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
 <script src="js/c3/c3.js"></script>
 <!--common script init for all pages-->
