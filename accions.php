5ac7284ba9cff3976cbf2b2db487017711d960b2
Pedro <pedrocch@fanbot.me>
2016-09-27 14:42:36 -0500

    *

--- accions.php
+++ accions.php
@@ -370,6 +370,19 @@ $(document).ready( function () {
 		                },
 		                "targets": 3
 		            },
+		            {
+		                // The `data` parameter refers to the data for the cell (defined by the
+		                // `data` option, which defaults to the column being worked with, in
+		                // this case `data: 0`.
+		                "render": function ( data, type, row ) {
+			                if ( data.indexOf("MMM") != -1){
+		                    	return "Mobile";				                
+			                } else {
+		                    	return "Classic";				                
+			                }
+		                },
+		                "targets": 6
+		            },
 		            { "visible": false,  "targets": [ 4 ] }
 		        ],
 			} );
