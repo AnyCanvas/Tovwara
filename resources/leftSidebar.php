3aa612b03c172d366be4eb088d3dc45ad400fac9
Pedro <pedrocch@fanbot.me>
2016-09-27 11:24:16 -0500

    *

--- resources/leftSidebar.php
+++ resources/leftSidebar.php
@@ -13,7 +13,7 @@
             <li class="sub-menu">
                 <a href="javascript:;">
                     <i class="fa fa-laptop"></i>
-                    <span>Estadísticas</span>
+                    <span>Fanbot Classic</span>
                 </a>
                 <ul class="sub">  
                     <li><a href="stats.php">Generales</a></li>
@@ -23,19 +23,6 @@
                 </ul>
             </li>
 
-
-            <li class="sub-menu">
-                <a href="javascript:;">
-                    <i class="fa fa-laptop"></i>
-                    <span>Estadísticas</span>
-                </a>
-                <ul class="sub">  
-                    <li><a href="stats.php">Generales</a></li>
-                    <li><a href="accions.php">Acciones</a></li>
-                    <li><a href="users.php">Usuarios</a></li>
-                    <li><a href="rate.php">Encuestas</a></li>
-                </ul>
-            </li>
             <li>
                 <a href="list.php">
                     <i class="fa fa-cogs"></i>
