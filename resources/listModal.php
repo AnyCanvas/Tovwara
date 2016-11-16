1858f4e9d45dd4c9b7cd722497f508e9c5626e00
Pedro <casillaschavezjuanpedro@gmail.com>
2016-11-16 15:28:27 -0600

    *

--- resources/listModal.php
+++ resources/listModal.php
@@ -63,6 +63,17 @@
 			location.reload();
 
 	    }   
+	function pricesAction(){
+
+	        var ajaxurl = 'resources/priceUrl.php';
+   	          data =  {'p1' : encodeURIComponent( $( "#p1" ).val() ), 'p2': encodeURIComponent( $( "#p2" ).val() ), 'p3': encodeURIComponent( $( "#p3" ).val() ), 'p4': encodeURIComponent( $( "#p4" ).val() ), 'p5': encodeURIComponent( $( "#p5" ).val() ), 'p6': encodeURIComponent( $( "#p6" ).val() ), 'p7': encodeURIComponent( $( "#p7" ).val() ), 'p8': encodeURIComponent( $( "#p8" ).val() )};		    		        
+	        console.log(data);	        
+	        $.post(ajaxurl, data, function (response) {
+	        });
+			$('#priceModal').modal('hide');
+			location.reload();
+
+	    }   
 </script>
 
 		  <div class="modal fade" id="configModal" role="dialog">
@@ -198,7 +209,7 @@
 			    <div class="modal-footer">
 				    <div class="center-block">
 					<p>
-					<button onclick="action()" id="cambiarBtn" class="btn btn-primary btn-sm" disabled="disabled">Cambiar</button>
+					<button onclick="pricesAction()" id="cambiarBtn" class="btn btn-primary btn-sm" disabled="disabled">Cambiar</button>
 					</p>
 		        </div>
 		      
